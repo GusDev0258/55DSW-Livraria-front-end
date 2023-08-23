9cba6433c46f7c10987b0d6f9a48b086e85060ff
gusdev0258 <16326176760@edu.udesc.br>
2023-08-23 14:35:50 -0300

    feat: created folders, Header and Home components :smile:

--- library-project/src/main.tsx
+++ library-project/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Router from './main/routes/Router'
+import { Header } from './presentation/components'
+import './presentation/styles/global/reset.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Header/>
+    <Router />
   </React.StrictMode>,
 )
---
+++ library-project/src/presentation/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { StyledHeader } from '../../styles/styled-components/header/styled-header'
+
+const Header = () => {
+
+  return (
+    <StyledHeader>
+      Library
+    </StyledHeader>
+  )
+}
+
+export default Header
